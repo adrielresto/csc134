13c32d8aa40938098c88ddb04ac8ca42f94df20b
honda <124730032+adrielresto@users.noreply.github.com>
2024-01-29 16:36:04 +0000

    M2T1 finished

---
+++ M2T1_Resto.cpp
@@ -0,0 +1,39 @@
+/*
+CSC 134
+M2T1 - Receipt
+Adriel J. Resto Barbosa
+1/29/24
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "M2T1" << endl;
+    cout << "Thank you for dining with us" << endl;
+    cout << "----------------------------" << endl;
+    // set up variables
+    string meal = "Value Meal";
+    // all in  $, except the percent
+    double meal_price = 5.99;
+    double tax_percent = 0.08;
+    double tax_amount = 0;
+    double total = 0;
+    // do calculations
+    tax_amount = meal_price * tax_percent;
+    total = meal_price + tax_amount;
+
+
+    //print the receipt
+    // print this once to set the decimals to exactly 2
+    cout << fixed << setprecision(2);
+    cout << meal  << "\t$" << meal_price << endl;
+    cout << "Tax" << "\t\t$" << tax_amount << endl;
+    cout << "----------------------------" << endl;
+    cout << "Total:" << " \t\t$" << total << endl;
+    
+    return 0;
+} 
